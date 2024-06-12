6f6cea922071f25e2eb14e281792be5186edca64
Ruveydabal <ruveydabal024@gmail.com>
2024-06-12 14:05:19 +0200

    first
    proberen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using DogAtTheRaces.classes;
+using Microsoft.Win32.SafeHandles;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,91 @@ namespace DogAtTheRaces
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        public Greyhound[] GreyhoundArray = new Greyhound[4];
+        public Guy[] guys = new Guy[3];
         public MainWindow()
         {
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            // Instellen wat de interval is (1 seconde)
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
             InitializeComponent();
+            Random Randomizer = new Random();
+            GreyhoundArray[0] = new Greyhound((int)dog1.Margin.Left, (int)(raceTrack.Width - dog1.Width), Randomizer, dog1)
+            {
+               
+            };
+            GreyhoundArray[1] = new Greyhound((int)dog2.Margin.Left, (int)(raceTrack.Width - dog2.Width), Randomizer, dog2)
+            {
+
+            };
+            GreyhoundArray[2] = new Greyhound((int)dog3.Margin.Left, (int)(raceTrack.Width - dog3.Width), Randomizer, dog3)
+            {
+
+            };
+            GreyhoundArray[3] = new Greyhound((int)dog4.Margin.Left, (int)(raceTrack.Width - dog4.Width), Randomizer, dog4)
+            {
+
+            };
+
+         
+
+            GreyhoundArray[0].TakeStartingPosition();
+            GreyhoundArray[1].TakeStartingPosition();
+            GreyhoundArray[2].TakeStartingPosition();
+            GreyhoundArray[3].TakeStartingPosition();
+
+            guys[0] = new Guy("Joe", 50, radioButton_Joe)
+            {
+                myLabel = label_Joe
+            };
+            guys[1] = new Guy("Bob", 75, radioButton_Bob)
+            {
+                myLabel = label_Bob
+            };
+            guys[2] = new Guy("Al",45, radioButton_Al)
+            {
+                myLabel = label_Al
+            };
+
+            guys[0].UpdateLabels();
+            guys[1].UpdateLabels();
+            guys[2].UpdateLabels();
+
+
+           
+        }
+
+
+
+
+        private void dispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Start();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            guys[0].PlaceBet(10, 1);
+            guys[0].UpdateLabels();
         }
     }
 }
--- classes/Bet.cs
+++ classes/Bet.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Automation;
+using System.Windows.Navigation;
 
 namespace DogAtTheRaces.classes
 {
-    internal class Bet
+    public class Bet
     {
+        private int amount;  //the amound of the cash that was bet
+        private int dog;  //the number of the dog the bet is on
+        private Guy bettor;  //the guy who placed the bet
+
+
+        public Bet(int betAmount, int dogToWin, Guy bettor)        
+        {
+            this.amount = betAmount;
+            this.dog = dogToWin;
+            this.bettor = bettor;
+
+        }
+
+        public string? GetDescription()
+        {
+            string description;
+
+            if (this.amount == 0)
+            {
+                description = bettor.name + "hasn't placed a bet";
+            }
+            else
+            {
+                 description = bettor.name + "bets" + this.amount + "on dog" + this.dog;
+
+            }
+
+            //return a string that says who placed the bet, how much
+            //cash was bet, and which dog he bet on ("joe bets 8 on dog #4").
+            //if the amound is zero, no bet was placed ("joe hasn't placed a bet").
+            return description;
+        }
+
+        public int PayOut(int winner)
+        {
+            
+            int payOut;
+
+            if (this.dog == winner)
+            {
+                Console.Write("the dog won");
+                payOut = amount;      
+            }
+            else
+            {
+                Console.Write("the dog lost");
+                payOut = -amount;   
+            }
+
+            //the parameter is the winner of the race. if the dog won, 
+            //return the amound bet. otherwise, return the negative of the amound bet.
+            return payOut;
+        }
+
     }
 }
