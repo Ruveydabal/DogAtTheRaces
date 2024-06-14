e6c50799ed10a0c4183f5ba517fb64f5868be9d6
Ruveydabal <ruveydabal024@gmail.com>
2024-06-14 14:01:57 +0200

    vrijdag
    vrijdag

--- classes/Greyhound.cs
+++ classes/Greyhound.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Xaml.Schema;
 
 namespace DogAtTheRaces.classes
 {
-    internal class Greyhound
+    public class Greyhound
     {
+        public int startingPosition; // where image starts
+        public int racetracklength; //length of racetrack
+        public Image? myPictureBox = null; //my image object
+        public int location = 0; //my location on the racetrack
+        public Random Randomizer; //an instance of random
+        TransformGroup trGrp = new TransformGroup();
+        TranslateTransform trTrf = new TranslateTransform();
+
+
+        public Greyhound(int startingPosition, int racetracklength, Random Randomizer, Image myPictureBox)
+        {
+            this.startingPosition = startingPosition;
+            this.racetracklength = racetracklength;
+            this.Randomizer = Randomizer;
+            this.myPictureBox = myPictureBox;
+            trGrp.Children.Add(trTrf);
+            this.myPictureBox.RenderTransform = trGrp;
+        }
+
+     
+        public bool Run()
+        {
+            int move = Randomizer.Next(1, 4);    //move foreward either 1,2,3 or 4 spaces at random
+            location = location + move;
+
+            trTrf.X = location;
+            trTrf.X = startingPosition + location;   
+
+            if(trTrf.X > 620)          //klopt dit?
+            {                   
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+            //update the posistion of my picturebox on the form like:
+            //      mypicturebox.left = startingposistion + location;
+
+            //return true if i won the game.
+        }
+
+        public void TakeStartingPosition()
+        {
+            location = 0;
+            myPictureBox = null; 
+
+            //reset my location to 0 and my picturebox to starting position
+        }
+         
+
     }
+
+   
 }
--- classes/Guy.cs
+++ classes/Guy.cs
@@ -1,12 +1,81 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Converters;
 
-namespace DogAtTheRaces.classes
+namespace DogAtTheRaces.classes 
 {
-    internal class Guy
+    public class Guy
     {
-    }
+        public string name;  //guy's name
+        public Bet? myBet = null;  //an instance of bet that has his bet
+        public int cash;   //how much cash he has 
+
+        public RadioButton? myRadioButton; //my radio button
+        public System.Windows.Controls.Label? myLabel = null;  //my label
+
+        public Guy(string name, Bet myBet, int myCash)
+        {
+            this.name = name;
+            this.myBet = myBet;
+            this.cash = myCash;
+        }
+        public void UpdateLabels()
+        {
+            
+            myLabel.Content = myBet.GetDescription();
+            myRadioButton.Content = name + "has" + cash + "bucks";
+
+            //set my label to my bet's description, and the label on my
+            //radio button to show my cash("Joe has 43 bucks")
+        }
+        public void ClearBet()
+        {
+            myBet = null;
+            //reset my bet to zero
+        }
+        public bool PlaceBet(int betAmount, int dogToWin)
+        {
+            myBet = new Bet(betAmount, dogToWin, this);
+
+            if (betAmount < 5 | betAmount > cash)
+            {
+                Console.Write("not enough cash");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+            //place a new bet and store it in my bet field
+            //return true if the guy had enough money to bet
+            //bets are represented by instances of Bet
+
+        }
+        public void Collect(int winner)
+        {
+            //myBet moet betalen
+
+            myBet.PayOut(winner);
+
+            ClearBet();
+            UpdateLabels();
+           //het geld moet naar winner
+          
+            //ask my bet to pay out, clear my bet, and update my labels
+            //use bet object
+        }
+
+     }
+   
 }
+
+
